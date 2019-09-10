ca6c3f6300563d3dfe47bfd0c91f78f09d7e84e9
mounisha <mounisha.resileo@gmail.com>
2019-09-10 17:57:19 +0530

    Final commit

--- src/main/java/resources/Base.java
+++ src/main/java/resources/Base.java
@@ -1,5 +1,6 @@
 package resources;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -7,6 +8,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,6 +17,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.apache.commons.io.FileUtils;
 
 
 public class Base {
@@ -93,7 +97,11 @@ public class Base {
 			{
 				driver.close();
 			}
-
+			
+			public void takeScreenshot(String result) throws Exception {
+		        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		        FileUtils.copyFile(scrFile, new File("F:\\Automation-Selenium\\TechUs\\Screenshots"+result+" "+System.currentTimeMillis()+".png"));
+		    }
 
 }
 
--- src/test/java/practice/TechUs/ContactPage.java
+++ src/test/java/practice/TechUs/ContactPage.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import Pages.Home_Page;
@@ -12,10 +14,18 @@ import resources.Base;
 public class ContactPage extends Base {
 	 public static Logger log = LogManager.getLogger(ArtificialInteligencePage.class.getName());
 	@Test
-	public void pageTitleVerify() throws IOException, InterruptedException {
+	public void pageTitleVerify() throws Exception {
 		Home_Page page=new Home_Page(driver);
-		menu(page.contactMenu(),"Tell Us About Your Dream. Let's chat! - Tech.us");
+		menu(page.contactMenu(),"Tell Us About Your Dream. Let's chat! - Tech.us1");
+		//takeScreenshot();
 		log.info("verified title");
 }
-
+@AfterMethod
+public void Screenshot(ITestResult result) throws Exception
+{
+	if(ITestResult.FAILURE==result.getStatus())
+	{
+		takeScreenshot();
+	}
+}
 }
